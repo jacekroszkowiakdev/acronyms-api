a7f9e885dd57ab44f7acc5cf210c69c3bac3ffcf
Jacek Roszkowiak <jacekroszkowiakdev@gmail.com>
2021-09-08 16:59:23 +0200

    app skeleton created

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const port = 3000;
 
+// set up the DB
+const mongoose = require("mongoose");
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on("error", (err) => console.error(err));
+db.once("open", () => console.log("connected to DB"));
+
 // Middleware
-var createError = require("http-errors");
+const createError = require("http-errors");
+const debug = require("debug");
+app.use(express.json());
+
+// routes:
 
 app.get("/", (req, res) => {
     res.send("I am a server!");
 });
+
+const appRouter = require("./routes/acronyms");
+app.use("/test", appRouter);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}!`);
---
+++ routes/acronyms.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const router = express.Router();
+
+// 1.
+// GET /acronym?from=50&limit=10&search=:search
+// ▶ returns a list of acronyms, paginated using query parameters
+// ▶ response headers indicate if there are more results
+// ▶ returns all acronyms that fuzzy match against :search
+router.get("/acronym?from=50&limit=10&search=:search", (req, res) => {});
+
+// 2.
+// GET /acronym/:acronym
+// ▶ returns the acronym and definition matching :acronym
+router.get("/acronym/:acronym", (req, res) => {});
+
+// 3.
+// GET /random/:count?
+// ▶ returns :count random acronyms
+// ▶ the acronyms returned should not be adjacent rows from the data
+router.get("/random/:count?", (req, res) => {});
+
+// 4.
+// POST /acronym
+// ▶ receives an acronym and definition strings
+// ▶ adds the acronym definition to the db
+router.post("/acronym", (req, res) => {});
+
+// 5.
+// PUT /acronym/:acronym
+// ▶ receives an acronym and definition strings
+// ▶ uses an authorization header to ensure acronyms are protected
+// ▶ updates the acronym definition to the db for :acronym
+router.patch("/acronym/:acronym", (req, res) => {});
+
+//  6.
+// DELETE /acronym/:acronym
+// ▶ deletes :acronym
+// ▶ uses an authorization header to ensure acronyms are protected
+router.delete("/acronym/:acronym", (req, res) => {});
+
+module.exports = router;
