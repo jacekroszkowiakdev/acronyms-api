729441906adbc48a2d2bb8ad1ea66acebb6da5c8
Jacek Roszkowiak <jacekroszkowiakdev@gmail.com>
2021-09-18 21:04:24 +0200

    auth.js removed from gitignore

---
+++ auth.js
@@ -0,0 +1,17 @@
+const jwt = require("jsonwebtoken");
+
+const auth = (req, res, next) => {
+    const bearerHeader = req.headers["authorization"];
+    console.log(req.headers);
+    if (bearerHeader) {
+        const bearer = bearerHeader.split(" ");
+        console.log(bearer);
+        const bearerToken = bearer[1];
+        req.token = bearerToken;
+        next();
+    } else {
+        res.sendStatus(403);
+    }
+};
+
+module.exports = auth;
